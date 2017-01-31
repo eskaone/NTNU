2c6ec2cdbf35f0fe0fe0bb7fd1861eedd0fbb24b
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2017-01-31 12:32:55 +0100

    ish working oving1, idk exactly what to do from here

--- 2017V/TDAT2004(datakom_nettprog)/Oving1/main.cpp
+++ 2017V/TDAT2004(datakom_nettprog)/Oving1/main.cpp
@@ -9,44 +9,42 @@ using namespace std;
 
 bool isPrime(int x) {
   int TOP = static_cast<int>(sqrt(static_cast<double>(x))) + 1;
-  for ( int i=2; i != TOP; ++i ) {
-    if (x % i == 0) return false;
+  for (int i = 2; i != TOP; ++i) {
+    if (x % i == 0)
+      return false;
   }
   return true;
 }
 
-/*
-vector<int> findPrimes(int from, int to) {
-
+void findPrimes(int from, int to) {
   vector<int> primes;
   
   for (int i = from; i <= to; i++) {
-    if(isPrime(i)) {
+    if (isPrime(i)) {
       primes.emplace_back(i);
     }
   }
-  return primes;
+  cout << "Find all primes between " << from << " - " << to << ":" << endl;
+  for (int i = 0; i < (int)primes.size(); i++) {
+    cout << primes.at(i) << endl;
+  }
 }
 
-vector<thread> createThreads(int amt) {
+void findPrimesWithThreads(int from, int to, int amt) {
   vector<thread> threads;
-  
-  for(int i = 0; i < amt; ++i) {
-    threads.emplace_back(thread([&](){
-      // task??
+  mutex primes_mutex;
+
+  for (int i = 0; i < amt; ++i) {
+    threads.emplace_back(thread([&] {
+      lock_guard<mutex> lock(primes_mutex);
+      findPrimes(from, to);
     }));
   }
-  return threads;
-}
-
-void execute(int from, int to, int threadAmt) {
-  vector<thread> threads = createThreads(threadAmt);
-  for(int i = 0; i < (int)threads.size(); i++) {
-    //add the calculate prime method to threads
-    //threads.at(i) 
+  for (int i = 0; i < (int)threads.size(); i++) {
+    threads.at(i).join();
   }
 }
-*/
+
 
 // for(int i = 0; i < (int)threads.size(); i++) {
 //   threads.at(i).join();
@@ -56,44 +54,86 @@ void execute(int from, int to, int threadAmt) {
 //   cout << primes.at(i) << endl;
 // }
 
-//mutex primes_mutex;
-//lock_guard<mutex> lock(primes_mutex);
 
-//cout << "Find all primes between " << from << " - " << to << ":\n";
+// void findPrimes(int from, int to, int threadAmt) {
+//   cout << "Find all primes between " << from << " - " << to << ":\n";
+//   vector<int> primes;
+//   vector<thread> threads;
+//   mutex primes_mutex;
 
+//   for(int i = 0; i < threadAmt; ++i) {
+//     threads.emplace_back(thread([&](){
+//       for (int i = from; i <= to; i++) {
+//         if(isPrime(i)) {
+//           lock_guard<mutex> lock(primes_mutex);
+//           primes.emplace_back(i);
+//         }
+//       }
+//     }));
+//   }
 
+//   for(int i = 0; i < (int)threads.size(); i++) {
+//     threads.at(i).join();
+//   }
 
-
-
-void findPrimes(int from, int to, int threadAmt) {
-  cout << "Find all primes between " << from << " - " << to << ":\n";
-  vector<int> primes;
-  vector<thread> threads; 
-  mutex primes_mutex;
-  
-  for(int i = 0; i < threadAmt; ++i) {
-    threads.emplace_back(thread([&](){
-      for (int i = from; i <= to; i++) {
-        if(isPrime(i)) {
-          lock_guard<mutex> lock(primes_mutex);
-          primes.emplace_back(i);
-        }
-      }
-    }));
-  }
-  
-  for(int i = 0; i < (int)threads.size(); i++) {
-    threads.at(i).join();
-  }
-  
-  for(int i = 0; i < (int)primes.size(); i++) {
-    cout << primes.at(i) << endl;
-  }
-}
-
-
+//   for(int i = 0; i < (int)primes.size(); i++) {
+//     cout << primes.at(i) << endl;
+//   }
+// }
 
 
 int main() {
-  findPrimes(2, 30, 2);
+  // findPrimes(2, 300);
+  findPrimesWithThreads(2, 100, 4);
+
+
+  // vector<int> primes;
+  // mutex primes_mutex;
+
+
+  // thread t1 = thread([&]() {
+  //   for (int i = 2; i <= 30; i++) {
+  //     if (isPrime(i)) {
+  //       for(int j = 0; j < (int)primes.size(); j++) {
+  //         if(primes.at(j) != i) {
+  //           lock_guard<mutex> lock(primes_mutex);
+  //           primes.emplace_back(i);
+  //         }
+  //       }
+        
+  //     }
+  //   }
+  // });
+
+  // thread t2 = thread([&]() {
+  //   for (int i = 2; i <= 30; i++) {
+  //     if (isPrime(i)) {
+  //       for(int j = 0; j < (int)primes.size(); j++) {
+  //         if(primes.at(j) != i) {
+  //           lock_guard<mutex> lock(primes_mutex);
+  //           primes.emplace_back(i);
+  //         }
+  //       }
+        
+  //     }
+  //   }
+  // });
+
+  // thread t3 = thread([&]() {
+  //   for (int i = 2; i <= 30; i++) {
+  //     if (isPrime(i)) {
+  //       lock_guard<mutex> lock(primes_mutex);
+  //       primes.emplace_back(i);
+  //     }
+  //   }
+  // });
+
+  // t1.join();
+  // t2.join();
+  // t3.join();
+
+  // cout << "Find all primes between " << 2 << " - " << 30 << ":\n";
+  // for (int i = 0; i < (int)primes.size(); i++) {
+  //   cout << primes.at(i) << "\n";
+  // }
 }
